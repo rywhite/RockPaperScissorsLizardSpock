d7a1474d8f62685281914025d7213bfa44126033
rywhite <rwhit29@gmail.com>
2021-01-29 16:08:27 -0700

    Add files via upload

---
+++ rpsls.py
@@ -0,0 +1,64 @@
+import random
+
+combos = {'Rock': {'Scissors': 'crushes', 'Lizard': 'crushes'},
+                 'Paper':{'Rock': 'covers','Spock': 'disproves'},
+                 'Scissors': {'Paper': 'cuts', 'Lizard': 'decapitates'},
+                 'Lizard': {'Paper': 'eats', 'Spock': 'poisons'},
+                 'Spock': {'Rock': 'vaporizes', 'Scissors': 'smashes'}}
+                 
+hands = {1: 'Rock', 2: 'Paper', 3: 'Scissors', 4: 'Lizard', 5: 'Spock'}
+
+history = []
+
+
+class Options:
+    
+    def throw(self):
+        return hands[random.randrange(1,6)]
+
+    def check_result(self):
+        op_throw = self.throw()
+        
+        if op_throw in combos[self.name]:
+            print(f'{self.name} {combos[self.name][op_throw]} {op_throw}. You win.')
+            return history.append((self.name, op_throw, 'win'))
+            
+        elif op_throw == self.name:
+            print("It's a tie.")
+            return history.append((self.name, op_throw, 'tie'))
+            
+        else:
+            print(f'{op_throw} {combos[op_throw][self.name]} {self.name}. You lose.')
+            return history.append((self.name, op_throw, 'loss'))
+
+
+class Rock(Options):
+    def __init__(self):
+        self.name = 'Rock'
+
+
+class Paper(Options):
+    def __init__(self):
+        self.name = 'Paper'
+
+
+class Scissors(Options):
+    def __init__(self):
+        self.name = 'Scissors'
+
+
+class Lizard(Options):
+    def __init__(self):
+        self.name = 'Lizard'
+
+
+class Spock(Options):
+    def __init__(self):
+        self.name = 'Spock'
+
+
+rock = Rock()
+paper = Paper()
+scissors = Scissors()
+lizard = Lizard()
+spock = Spock()
